76cef6c2545a57f321387b636953df20e5737fdf
pek5387 <a01053878389@gmail.com>
2023-03-21 16:02:47 +0900

    Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
 import { Button, Flex, Grid } from "./Common/UI/Atoms";
 import styled from "styled-components";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Wrapper column={4} row={4}>
-                <Flex column rowArea={"1/4"} fill>
-                    test1
-                    <Flex fill />
-                </Flex>
-                <Flex
-                    column
-                    rowArea={"2/5"}
-                    columnArea={"1/4"}
-                    padding={["20px", undefined, "40px"]}
-                    align={"center"}
-                    fill
-                >
-                    <Button fill onClick={() => alert("test")}>
-                        test
-                    </Button>
-                </Flex>
-            </Wrapper>
+            <Routes>
+                <Route
+                    path="*"
+                    element={
+                        <Wrapper column={4} row={4}>
+                            <Flex column rowArea={"1/4"} fill>
+                                test1
+                                <Flex fill />
+                            </Flex>
+                            <Flex
+                                column
+                                rowArea={"2/5"}
+                                columnArea={"1/4"}
+                                padding={["20px", undefined, "40px"]}
+                                align={"center"}
+                                fill
+                            >
+                                <Button fill to={"/dd"}>
+                                    test
+                                </Button>
+                            </Flex>
+                        </Wrapper>
+                    }
+                />
+                <Route path={"/dd"} element={<>테스트 진행</>} />
+            </Routes>
         </div>
     );
 }
